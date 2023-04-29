7b649e9e118a03769624ec3660097055823629d8
LueKedowsky <vinipuh041298.ru@ya.ru>
2023-04-29 17:57:30 +0300

    YAMR-1 Fix header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,70 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <header className="header">
-        <div className="container-header">
-          <div className="logo">
-            <a href="/">
-              <p>YAMANEKO</p>
-            </a>
-          </div>
-          <div className="search">
-            <input type="search" name="" id="" placeholder="Поиск..." />
-          </div>
-          <nav className="navigation">
-            <ul>
-              <li>
-                <Link to="/project">Проект</Link>
-              </li>
-              <li>
-                <Link to="/releases">Релизы</Link>
-              </li>
-              <li>
-                <a href="/support">Поддержка</a>
-              </li>
-              <li className="dropdown">
-                <button className="dropbtn">
-                  Социальные сети
-                  <img className="dropbtn" src="../../data/assets/icons/select_button.svg" alt="" />
-                </button>
-                <div id="myDropdown" className="dropdown-content">
-                  <a href="#">Instagram</a>
-                  <a href="#">Discord</a>
-                  <a href="#">Telegram</a>
-                  <a href="#">Youtube</a>
-                </div>
-              </li>
-            </ul>
-          </nav>
-          <div className="sign-in">
-            Войти
-            {/* <!-- <p><a className="open_auth" href="#open">ВОЙТИ</a></p> -->
-                <!-- Временная кнопка для завершения сессии -->
-                <p><a href="../../private/lib/exit.php">выйти</a></p>
-                <?php
-                    switch(isset($_SESSION['logged_in'])){
-                        case 0 : {
-                            // Если не авторизован - кнопка войти
-                            echo("<p><a href=\"sign-in\">ВОЙТИ</a></p>");
-                            break;
-                        }
-                        case 1 : {
-                            //для удобства нажатие на кнопку переводит в админку
-                            echo("<p><a href=\"admin\">АВТОРИЗОВАН</a></p>");
-                            break;
-                        }
-                        default : {
-                            echo ("<p><a href=\"sign-in\">ВОЙТИ</a></p>");
-                        }
-                    }
-                ?> */}
-          </div>
-        </div>
-      </header>
-    </div>
+    <header className="header-container">
+      <div className="header-container-logo">
+        <Link to="/">
+          <p>YAMANEKO</p>
+        </Link>
+      </div>
+      <div className="header-container-search">
+        <input className="header-container-search__input" type="search" name="" id="" placeholder="Поиск..." />
+      </div>
+      <nav className="header-container-navigation">
+        <ul className="header-container-navigation__ul">
+          <li className="header-container-navigation__li">
+            <NavLink to="/project">Проект</NavLink>
+          </li>
+          <li className="header-container-navigation__li">
+            <NavLink to="/releases">Релизы</NavLink>
+          </li>
+          <li className="header-container-navigation__li">
+            <NavLink to="/support">Поддержка</NavLink>
+          </li>
+          {/* <li className="dropdown">
+            <button className="dropbtn">
+              Социальные сети
+              <img className="dropbtn" src="../../data/assets/icons/select_button.svg" alt="" />
+            </button>
+            <div id="myDropdown" className="dropdown-content">
+              <a href="#">Instagram</a>
+              <a href="#">Discord</a>
+              <a href="#">Telegram</a>
+              <a href="#">Youtube</a>
+            </div>
+          </li> */}
+        </ul>
+      </nav>
+      <div className="header-container-sign-in">
+        <Link to="/sign-in">войти</Link>
+      </div>
+    </header>
   );
 };
 
