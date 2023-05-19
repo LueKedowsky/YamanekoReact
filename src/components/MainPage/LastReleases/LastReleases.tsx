96a2b97e9c699f6505fd5ada70e0e328be34fd2f
LueKedowsky <vinipuh041298.ru@ya.ru>
2023-05-19 17:18:09 +0300

    YAMR-9 Add new LastReleasesRelease component. Refactor LastReleases component. Fix styles.

--- src/components/MainPage/LastReleases/LastReleases.tsx
+++ src/components/MainPage/LastReleases/LastReleases.tsx
@@ -3,63 +3,19 @@ import "./LastReleases.scss";
 import data from "../../../data/tempData/data.json";
 import { SingleReleaseDataType } from "../../../Types/dataTypes";
 import LastReleasesBanner from "./LastReleasesBanner/LastReleasesBanner";
-import { Link } from "react-router-dom";
+import LastReleasesRelease from "./LastReleasesRelease/LastReleasesRelease";
 
 const LastReleases: React.FC = () => {
   const releaseData: SingleReleaseDataType[] = JSON.parse(JSON.stringify(data));
+  const fourElementsOfReleaseData: SingleReleaseDataType[] = releaseData.slice(0, 4);
   return (
     <div className="lastReleases-container">
       <div className="lastReleases-container--inner">
         <div className="lastReleases-title">ПОСЛЕДНИЕ РЕЛИЗЫ</div>
         <div className="lastReleases-releases">
-          <div className="lastReleases-releases_item">
-            <div className="lastReleases-releases_item-poster">
-              <Link to={releaseData[0].titleLink}>
-                <img src={releaseData[0].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
-              </Link>
-            </div>
-            <div className="lastReleases-releases_item-title">
-              <Link className="lastReleases-releases_item-title_link" to={releaseData[0].titleLink}>
-                {releaseData[0].releaseTitleRu}
-              </Link>
-            </div>
-          </div>
-          <div className="lastReleases-releases_item">
-            <div className="lastReleases-releases_item-poster">
-              <Link to={releaseData[1].titleLink}>
-                <img src={releaseData[1].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
-              </Link>
-            </div>
-            <div className="lastReleases-releases_item-title">
-              <Link className="lastReleases-releases_item-title_link" to={releaseData[1].titleLink}>
-                {releaseData[1].releaseTitleRu}
-              </Link>
-            </div>
-          </div>
-          <div className="lastReleases-releases_item">
-            <div className="lastReleases-releases_item-poster">
-              <Link to={releaseData[2].titleLink}>
-                <img src={releaseData[2].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
-              </Link>
-            </div>
-            <div className="lastReleases-releases_item-title">
-              <Link className="lastReleases-releases_item-title_link" to={releaseData[2].titleLink}>
-                {releaseData[2].releaseTitleRu}
-              </Link>
-            </div>
-          </div>
-          <div className="lastReleases-releases_item">
-            <div className="lastReleases-releases_item-poster">
-              <Link to={releaseData[3].titleLink}>
-                <img src={releaseData[3].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
-              </Link>
-            </div>
-            <div className="lastReleases-releases_item-title">
-              <Link className="lastReleases-releases_item-title_link" to={releaseData[3].titleLink}>
-                {releaseData[3].releaseTitleRu}
-              </Link>
-            </div>
-          </div>
+          {fourElementsOfReleaseData.map(({ id, titleLink, releasePoster, releaseTitleRu }) => (
+            <LastReleasesRelease key={id} titleLink={titleLink} releasePoster={releasePoster} releaseTitleRu={releaseTitleRu} />
+          ))}
         </div>
         <LastReleasesBanner />
       </div>
