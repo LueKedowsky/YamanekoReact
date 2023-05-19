1e641806b30e1dc9a3a7112d31c7853c53d7ba70
LueKedowsky <vinipuh041298.ru@ya.ru>
2023-05-19 16:56:24 +0300

    YAMR-9 Add links to LastReleases

--- src/components/MainPage/LastReleases/LastReleases.tsx
+++ src/components/MainPage/LastReleases/LastReleases.tsx
@@ -3,6 +3,7 @@ import "./LastReleases.scss";
 import data from "../../../data/tempData/data.json";
 import { SingleReleaseDataType } from "../../../Types/dataTypes";
 import LastReleasesBanner from "./LastReleasesBanner/LastReleasesBanner";
+import { Link } from "react-router-dom";
 
 const LastReleases: React.FC = () => {
   const releaseData: SingleReleaseDataType[] = JSON.parse(JSON.stringify(data));
@@ -13,27 +14,51 @@ const LastReleases: React.FC = () => {
         <div className="lastReleases-releases">
           <div className="lastReleases-releases_item">
             <div className="lastReleases-releases_item-poster">
-              <img src={releaseData[0].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              <Link to={releaseData[0].titleLink}>
+                <img src={releaseData[0].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              </Link>
             </div>
-            <div className="lastReleases-releases_item-title">{releaseData[0].releaseTitleRu}</div>
+            <div className="lastReleases-releases_item-title">
+              <Link className="lastReleases-releases_item-title_link" to={releaseData[0].titleLink}>
+                {releaseData[0].releaseTitleRu}
+              </Link>
+            </div>
           </div>
           <div className="lastReleases-releases_item">
             <div className="lastReleases-releases_item-poster">
-              <img src={releaseData[1].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              <Link to={releaseData[1].titleLink}>
+                <img src={releaseData[1].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              </Link>
             </div>
-            <div className="lastReleases-releases_item-title">{releaseData[1].releaseTitleRu}</div>
+            <div className="lastReleases-releases_item-title">
+              <Link className="lastReleases-releases_item-title_link" to={releaseData[1].titleLink}>
+                {releaseData[1].releaseTitleRu}
+              </Link>
+            </div>
           </div>
           <div className="lastReleases-releases_item">
             <div className="lastReleases-releases_item-poster">
-              <img src={releaseData[2].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              <Link to={releaseData[2].titleLink}>
+                <img src={releaseData[2].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              </Link>
             </div>
-            <div className="lastReleases-releases_item-title">{releaseData[2].releaseTitleRu}</div>
+            <div className="lastReleases-releases_item-title">
+              <Link className="lastReleases-releases_item-title_link" to={releaseData[2].titleLink}>
+                {releaseData[2].releaseTitleRu}
+              </Link>
+            </div>
           </div>
           <div className="lastReleases-releases_item">
             <div className="lastReleases-releases_item-poster">
-              <img src={releaseData[3].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              <Link to={releaseData[3].titleLink}>
+                <img src={releaseData[3].releasePoster} alt="" className="lastReleases-releases_item-poster_img" />
+              </Link>
             </div>
-            <div className="lastReleases-releases_item-title">{releaseData[3].releaseTitleRu}</div>
+            <div className="lastReleases-releases_item-title">
+              <Link className="lastReleases-releases_item-title_link" to={releaseData[3].titleLink}>
+                {releaseData[3].releaseTitleRu}
+              </Link>
+            </div>
           </div>
         </div>
         <LastReleasesBanner />
